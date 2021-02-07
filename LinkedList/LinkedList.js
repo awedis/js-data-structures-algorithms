1781dd94fa0898f52e70ea8872e5ee28b0614812
Avo-Keofteian <avokeofteian@gmail.com>
2021-02-07 13:21:39 +0200

    LinkedList inset Node & print list

---
+++ LinkedList/LinkedList.js
@@ -0,0 +1,45 @@
+class Node {
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = next;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    // Insert first node
+    insertFirst(data) {
+        this.head = new Node(data, this.head);
+    }
+
+    // Insert last node
+
+    // Insert at index
+
+    // Get at index
+
+    // Remove at index
+
+    // Clear lisst
+
+    // Print list data
+    printListData() {
+        let current = this.head;
+
+        while(current) {
+            console.log(current.data);
+            current = current.next;
+        }
+    }
+}
+
+const ll = new LinkedList();
+ll.insertFirst(100);
+ll.insertFirst(200);
+ll.insertFirst(300);
+
+ll.printListData();
